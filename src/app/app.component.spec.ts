9c7c64408a3b441976f74566ba3c5d59e6950a51
Björn Wilmsmann <bjoern@wilmsmann.de>
2020-10-04 15:41:59 +0200

    Added additional components.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -24,12 +24,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Angular8-PrimeNG9-sample-application');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular8-PrimeNG9-sample-application app is running!');
   });
 });
--- src/app/form/form-routing.module.ts
+++ src/app/form/form-routing.module.ts
@@ -3,17 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { FormComponent } from './form.component';
 import { AutoCompleteComponent } from "./auto-complete/auto-complete.component";
+import { CalendarComponent } from "./calendar/calendar.component";
+import { InputsComponent } from "./inputs/inputs.component";
 
 const routes: Routes = [
   { path: '', component: FormComponent },
-  { path: 'auto-complete', component: AutoCompleteComponent }
+  { path: 'auto-complete', component: AutoCompleteComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'inputs', component: InputsComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
   declarations: [
-    AutoCompleteComponent
+    AutoCompleteComponent,
+    CalendarComponent,
+    InputsComponent
   ]
 })
 export class FormRoutingModule { }
