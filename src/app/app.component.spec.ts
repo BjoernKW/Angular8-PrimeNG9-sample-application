aae2c2334788bf5eb04768c7633f6ee53428aff3
Björn Wilmsmann <bjoern@wilmsmann.de>
2020-10-26 23:05:17 +0100

    Fixed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,14 +2,16 @@ import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './menu/main-menu/main-menu.component';
-import { MenubarModule } from 'primeng/menubar';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MenuModule } from 'primeng/menu';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        MenubarModule
+        MenuModule,
+        FontAwesomeModule
       ],
       declarations: [
         AppComponent,
